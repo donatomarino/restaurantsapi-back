fee8375de2db421438440e0868a17d44fe860b93
donatomarino <donato_8@icloud.com>
2025-07-24 02:11:38 +0200

    fix

--- config/cors.php
+++ config/cors.php
@@ -18,7 +18,7 @@ return [
     'paths' => ['api/*', 'sb-json', 'api/documentacion'],
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://restaurantsapi-back-1.onrender.com'],
+    'allowed_origins' => ['https://restaurantsapi-front.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
