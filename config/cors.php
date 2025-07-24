65c70038454c3606a46af35e7b25a251a2cdeb4f
donatomarino <donato_8@icloud.com>
2025-07-24 12:48:34 +0200

    fix swagger

--- config/cors.php
+++ config/cors.php
@@ -18,7 +18,7 @@ return [
     'paths' => ['api/*', 'sb-json', 'api/documentacion'],
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('CORS_ALLOWED_ORIGINS')],
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', '')),
     
     'allowed_origins_patterns' => [],
 
